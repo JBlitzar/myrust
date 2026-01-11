af37dc45c2f1a9431cb6a727cc085e406081f6cd
JBlitzar <90664831+JBlitzar@users.noreply.github.com>
2026-01-11 11:34:36 -0800

    wow

---
+++ rectangle/main.rs
@@ -0,0 +1,39 @@
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+fn area(rectangle: &Rectangle) -> u32 {
+    rectangle.width * rectangle.height
+}
+
+fn volume(cuboid: &Cuboid) -> u32 {
+    cuboid.width * cuboid.height * cuboid.depth
+}
+
+struct Cuboid {
+    width: u32,
+    height: u32,
+    depth: u32,
+}
+
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        area(&rect1)
+    );
+
+    let c1 = Cuboid {
+        width: 10,
+        height: 20,
+        depth: 15,
+    };
+
+    println!("The volume of the cuboid is {} voxels.", volume(&c1));
+}
+

ca5842de0cb711c0efb1440c91c1cd0bb4497753
JBlitzar <90664831+JBlitzar@users.noreply.github.com>
2026-01-14 10:09:45 -0800

    Some letter

--- hello_world/main.rs
+++ hello_world/main.rs
@@ -2,4 +2,9 @@ fn main() {
     let a = "Blue";
     println!("Hello, world!");
     println!("The color is: {}", a);
+    if let Some(letter) = a.chars().nth(2) {
+        println!("The letter at index 2 is: {}", letter);
+    }
+
+
 }

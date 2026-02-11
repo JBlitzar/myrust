d869fc427c0e386557223795d426b5738d2cf515
JBlitzar <90664831+JBlitzar@users.noreply.github.com>
2026-02-11 14:44:15 -0800

    sdafdsaf

---
+++ binradix/src/main.rs
@@ -0,0 +1,39 @@
+
+fn radix_sort(arr: &mut  Vec<i32> ){
+    // find the maximum number to know the number of digits
+    let max = arr.iter().max().unwrap().clone();
+    let mut exp = 0;
+    let mut new = vec![0; arr.len()];
+    let mut bitmask = vec![false; arr.len()];
+    while (max >> exp) > 0 {
+        sub_sort(arr, exp, &mut new, &mut bitmask);
+        exp += 1;
+    }
+
+}
+
+fn sub_sort(arr: &mut Vec<i32> , exp: i32, new: &mut Vec<i32>, bitmask: &mut Vec<bool>) {
+    // binary counting sort: scan through for all the zeros, add them to a new array, then scan through for all the ones, add them to the new array, then copy the new array back to the original array
+    new.clear();
+    for i in 0..arr.len() {
+        bitmask[i] = (arr[i] >> exp) & 1 == 1;
+        // if the bit at exp is 0, add to new array
+        if !bitmask[i] {
+            new.push(arr[i]);
+        }
+    }
+    for i in 0..arr.len() {
+        if bitmask[i] {
+            new.push(arr[i]);
+        }
+    }
+
+    arr.copy_from_slice(&new);
+}
+
+fn main() {
+    println!("Hello, world!");
+    let mut arr = vec![10,9,8,1,2,3,4,7,6,5,1_000_000, 999_999, 1_000_001];
+    radix_sort(&mut arr);
+    println!("{:?}", arr);
+}

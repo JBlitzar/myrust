4db7643192702d1b1e75cec47b393ae48464abc3
JBlitzar <90664831+JBlitzar@users.noreply.github.com>
2026-02-11 14:49:30 -0800

    Update main.rs

--- binradix/src/main.rs
+++ binradix/src/main.rs
@@ -1,30 +1,36 @@
 
-fn radix_sort(arr: &mut  Vec<i32> ){
+fn radix_sort(arr: &mut  Vec<u32> ){
+    if(arr.is_empty()) {
+        return;
+    }
     // find the maximum number to know the number of digits
     let max = arr.iter().max().unwrap().clone();
-    let mut exp = 0;
+    let mut exp: usize = 0;
     let mut new = vec![0; arr.len()];
     let mut bitmask = vec![false; arr.len()];
+    let mut counter: usize = 0;
     while (max >> exp) > 0 {
-        sub_sort(arr, exp, &mut new, &mut bitmask);
+        sub_sort(arr, exp, &mut new, &mut bitmask, &mut counter);
         exp += 1;
     }
 
 }
 
-fn sub_sort(arr: &mut Vec<i32> , exp: i32, new: &mut Vec<i32>, bitmask: &mut Vec<bool>) {
+fn sub_sort(arr: &mut Vec<u32> , exp: usize, new: &mut Vec<u32>, bitmask: &mut Vec<bool>, counter: &mut usize) {
     // binary counting sort: scan through for all the zeros, add them to a new array, then scan through for all the ones, add them to the new array, then copy the new array back to the original array
-    new.clear();
+    *counter = 0;
     for i in 0..arr.len() {
         bitmask[i] = (arr[i] >> exp) & 1 == 1;
         // if the bit at exp is 0, add to new array
         if !bitmask[i] {
-            new.push(arr[i]);
+            new[*counter] = arr[i];
+            *counter += 1;
         }
     }
     for i in 0..arr.len() {
         if bitmask[i] {
-            new.push(arr[i]);
+            new[*counter] = arr[i];
+            *counter += 1;
         }
     }
 
